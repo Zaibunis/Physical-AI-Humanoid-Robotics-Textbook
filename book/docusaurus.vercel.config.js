a9eb06da08b5041093cc792ee7e1b30af38699a3
Zaibunis <165600878+Zaibunis@users.noreply.github.com>
2025-12-05 16:29:23 +0500

    new-changes

---
+++ book/docusaurus.vercel.config.js
@@ -0,0 +1,6 @@
+const config = require('./docusaurus.config');
+
+config.url = 'https://vercel.com/faria-mustaqeems-projects/physical-ai-humanoid-robotics-textbook/G7G7sU2kfKR9v3Wur9FHCJdKHFER';
+config.baseUrl = '/';
+
+module.exports = config;
